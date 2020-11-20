33d304982ba7750f610711940c4767cfee2e4286
Etienne Chanudet <chanudet.etienne@gmail.com>
2020-11-20 20:58:35 +0100

    First commit
    Add project

---
+++ car.cpp
@@ -0,0 +1,83 @@
+#include "car.h"
+
+#include <Arduino.h>
+
+Car::Car(uint8_t sp): m_sp(sp), m_dir(RELEASE) 
+{
+  m_motor_back_l = new AF_DCMotor(1);
+  m_motor_back_r = new AF_DCMotor(2); 
+  m_motor_front_r = new AF_DCMotor(3); 
+  m_motor_front_l = new AF_DCMotor(4); 
+}
+
+Car::~Car()
+{
+  delete m_motor_back_l;
+  delete m_motor_back_r;
+  delete m_motor_front_l;
+  delete m_motor_front_r;
+}
+
+void Car::set_speed(uint8_t sp)
+{
+  if (sp > 0 && sp < 153)
+    sp = 153;
+  m_sp = sp;
+  m_motor_back_l->setSpeed(m_sp);
+  m_motor_back_r->setSpeed(m_sp);
+  m_motor_front_l->setSpeed(m_sp);
+  m_motor_front_r->setSpeed(m_sp);
+}
+
+void Car::set_direction(uint8_t dir)
+{
+  m_dir = dir;
+  m_motor_back_l->run(m_dir);
+  m_motor_back_r->run(m_dir);
+  m_motor_front_l->run(m_dir);
+  m_motor_front_r->run(m_dir);
+}
+
+void Car::turn_left()
+{
+  m_motor_back_l->setSpeed(m_sp/3);
+  m_motor_front_l->setSpeed(m_sp/3);
+  delay(500);
+  m_motor_back_l->setSpeed(m_sp);
+  m_motor_front_l->setSpeed(m_sp);
+}
+
+void Car::turn_right()
+{
+  m_motor_back_r->setSpeed(m_sp/3);
+  m_motor_front_r->setSpeed(m_sp/3);
+  delay(500);
+  m_motor_back_r->setSpeed(m_sp);
+  m_motor_front_r->setSpeed(m_sp);
+}
+
+void Car::rotate_left()
+{
+  m_motor_back_l->run(BACKWARD);
+  m_motor_front_l->run(BACKWARD);
+  m_motor_back_r->run(FORWARD);
+  m_motor_front_r->run(FORWARD);
+  delay(500);
+  m_motor_back_r->run(m_dir);
+  m_motor_front_r->run(m_dir);
+  m_motor_back_l->run(m_dir);
+  m_motor_front_l->run(m_dir);
+}
+
+void Car::rotate_right()
+{
+  m_motor_back_r->run(BACKWARD);
+  m_motor_front_r->run(BACKWARD);
+  m_motor_back_l->run(FORWARD);
+  m_motor_front_l->run(FORWARD);
+  delay(500);
+  m_motor_back_r->run(m_dir);
+  m_motor_front_r->run(m_dir);
+  m_motor_back_l->run(m_dir);
+  m_motor_front_l->run(m_dir);
+}
---
+++ car.h
@@ -0,0 +1,33 @@
+#ifndef CAR_H
+#define CAR_H
+
+#include <AFMotor.h>
+
+class Car
+{
+  public:
+  Car(uint8_t sp);
+  ~Car();
+
+  //Min value is 200 for work correctly (why ? I don't know)
+  //Max value is 255
+  void set_speed(uint8_t sp);
+
+  //RELEASE, FORWARD, BACKWARD
+  void set_direction(uint8_t dir);
+  void turn_left();
+  void turn_right();
+  void rotate_left();
+  void rotate_right();
+  
+  private:
+  
+  AF_DCMotor *m_motor_back_l;
+  AF_DCMotor *m_motor_back_r;
+  AF_DCMotor *m_motor_front_l;
+  AF_DCMotor *m_motor_front_r;
+  uint8_t m_sp;
+  uint8_t m_dir;
+};
+
+#endif /* !CAR_H */
